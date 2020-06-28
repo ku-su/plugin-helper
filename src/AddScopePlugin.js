4203b3320483b324fe1b2fcf688958088490fade
Elvn <50227733@qq.com>
2020-06-28 16:21:18 +0800

    直接调用window.top的cooshuHelper

--- src/AddScopePlugin.js
+++ src/AddScopePlugin.js
@@ -2,12 +2,12 @@ function addIndexScope(pluginName, fileContent) {
   return `
 (function(win) {
   function CooshuScope() {
-    window.cooshuHelper.cloneWindow(win, this);
+    window.top.cooshuHelper.cloneWindow(win, this);
   }
   CooshuScope.prototype.init = function() {
     var window = this;var module;
     ${fileContent}
-    window.cooshuHelper.libraryRegisterFromJsFile(window.library, module, '${pluginName}', 'index', document.currentScript.src);
+    window.top.cooshuHelper.libraryRegisterFromJsFile(window.library, module, '${pluginName}', 'index', document.currentScript.src);
   };
   new CooshuScope().init();
 })(window);`;
@@ -18,7 +18,7 @@ function addConfigScope(pluginName, fileContent) {
 (function(window) {
   var module;
   ${fileContent}
-  window.cooshuHelper.libraryRegisterFromJsFile(window.config, module, '${pluginName}','config', document.currentScript.src);
+  window.top.cooshuHelper.libraryRegisterFromJsFile(window.config, module, '${pluginName}','config', document.currentScript.src);
 })(window);`;
 }
 
@@ -31,22 +31,17 @@ class AddScopePlugin {
   apply(compiler) {
     compiler.plugin('emit', function(compilation, callback) {
       for (let filename in compilation.assets) {
-        if (filename === 'appViewer.js') {
-          callback();
-        }
-        else {
-          const fileContent = compilation.assets[filename].source();
-          const formatFileContent = returnNewFileContent(fileContent, filename);
+        const fileContent = compilation.assets[filename].source();
+        const formatFileContent = returnNewFileContent(fileContent, filename);
 
-          compilation.assets[filename] = {
-            source() {
-              return formatFileContent;
-            },
-            size() {
-              return formatFileContent.length;
-            },
-          };
-        }
+        compilation.assets[filename] = {
+          source() {
+            return formatFileContent;
+          },
+          size() {
+            return formatFileContent.length;
+          },
+        };
       }
       callback();
     });
