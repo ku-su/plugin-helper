2bbf890ae55d8e3867ba345eff79ca658f7687b8
Elvn <50227733@qq.com>
2020-07-09 16:04:59 +0800

    defineContainer的Option中的appendData不是必须的

--- src/IPlugin.ts
+++ src/IPlugin.ts
@@ -24,7 +24,7 @@ export interface IBindingContextData {
 
 export interface IOptions {
   defaultContent?: string,
-  appendData: IBindingContextData,
+  appendData?: IBindingContextData,
 }
 
 export type getType = (type: IType) => any
